e0082d18065273260bb8c4854892dc4555f31d6c
Beck Rivera <ber46@drexel.edu>
2019-05-11 16:59:04 -0400

    Aeat - The Beginning Fleshing out the initial Aeat skeleton.

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="main.css">
-        <title>FOOD WEBSITE TITLE</title>
-    </head>
-    <body>
-
-    </body>
-</html>
+<?php
+  include "includes/functions.php";
+  include "includes/_header.php";
+?>
+<?php include "includes/_footer.php"; ?>
